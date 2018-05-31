e629dbe2ed5caedff50731b2199be5d4be2ace58
Alex Beichuk <alexander.beichuk@gmail.com>
2018-05-31 08:49:19 +0300

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CabinetComponent } from './cabinet/cabinet.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'cabinet',
+    component: CabinetComponent
+  },
+  /*{
+   path: 'hero/:id',
+   component: HeroDetailComponent
+   },
+   {
+   path: 'heroes',
+   component: HeroListComponent,
+   data: { title: 'Heroes List' }
+   },
+   {
+   path: '**', component: PageNotFoundComponent
+   }*/
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CabinetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
